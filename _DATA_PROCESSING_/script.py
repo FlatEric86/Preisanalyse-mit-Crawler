841b4d8ac92222aa128f1e0bb08ca854beffa785
FlatErik1 <67578051+FlatErik1@users.noreply.github.com>
2020-06-29 13:00:49 +0200

    change correct store_id column names

--- _DATA_PROCESSING_/script.py
+++ _DATA_PROCESSING_/script.py
@@ -2,8 +2,18 @@ import pandas as pd
 import numpy as np
 
 # read prices data CSV fila as pandas data frame
-# the parameter <engine> is used to avoid encoding problemes 
-df = pd.read_csv('./prices.csv', delimiter=';',engine='python')
+# the parameter <engine> is used to avoid encoding problems 
+# furthermore we ignore the column of store_1
+df = pd.read_csv('./prices.csv',
+    delimiter=';',
+    engine='python',
+    usecols = ['article_name','store_0', 'store_2'])
 
 
-print(df.head())
+# ratio between the price of store_0 (Amazon) and store_2 (Toys For Fun)
+price_ratio      = df['store_0'] / df['store_2']
+
+# absolut price difference of store_0 (Amazon) minus store_2 (Toys For Fun)
+price_difference = abs(df['store_0'] - df['store_2'])
+
+print(price_difference)
